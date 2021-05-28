57295fee39d889e2353cd24e4736c0209f2f4812
Caique <caique.andrade@quicko.com.br>
2021-05-28 20:05:12 -0300

    Import assets and create activity_memory_game.xml

--- app/src/main/java/com/homermemorygame/ui/LobbyActivity.kt
+++ app/src/main/java/com/homermemorygame/ui/LobbyActivity.kt
@@ -1,15 +1,16 @@
 package com.homermemorygame.ui
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import com.homermemorygame.R
 import com.homermemorygame.databinding.ActivityLobbyBinding
 
-class LobbyActivity: AppCompatActivity() {
+
+class LobbyActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLobbyBinding
 
@@ -18,8 +19,9 @@ class LobbyActivity: AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_lobby)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.statusBarColor = ContextCompat.getColor(baseContext, android.R.color.white)
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            val wic = WindowInsetsControllerCompat(window, window.decorView)
+            wic.isAppearanceLightStatusBars = true
+            window.statusBarColor = Color.WHITE
         }
 
     }
